37c6039c62c9ea21406c4b26b3740733147d7cd7
405 <5637343@hanmail.net>
2024-08-13 15:14:30 +0900

    네트워크 클라이언트 파일

--- src/Client.java
+++ src/Client.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) throws IOException {
         boolean running=true;
-        Socket socket = new Socket("192.168.0.108",7777);
+        //Socket socket = new Socket("192.168.0.108",7777);
+        Socket socket = new Socket("127.0.0.1",7777);
         OutputStream out=socket.getOutputStream();
         InputStream in=socket.getInputStream();
         PrintWriter writer=new PrintWriter(out, true);
         BufferedReader reader=new BufferedReader(new InputStreamReader(in));
 
         while (running){
-            System.out.println(">> ");
+            System.out.println(">> ");//사용자 입력
             Scanner scanner=new Scanner(System.in);
             String strMsg=scanner.nextLine();
             writer.println(strMsg);
+            String strMsg2 = reader.readLine();//통신입력
+            System.out.println(strMsg2);
+
         }
     }
 }
